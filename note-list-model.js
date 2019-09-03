f821ed7be63a72ede92d130fd4e8886625364c5b
EvelinaClayton <clayton.evelina@gmail.com>
2019-09-03 11:19:54 +0100

    added note-list-model

---
+++ note-list-model.js
@@ -0,0 +1,19 @@
+(function(exports) {
+  
+    function NoteList() {
+        this._list = [];
+    };
+    
+    NoteList.prototype.create(note); {
+        this._list.push(note);
+    }
+
+
+    NoteList.prototype.view(); {
+        // loop through each and print out the .text?
+        this._list;
+    }
+
+    exports.NoteList = Note.prototype.constructor = NoteList;
+})(this);
+  
